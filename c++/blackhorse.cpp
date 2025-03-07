6975f75d48d5e617863b73e71ce152b4d346c274
lyl <13416088820@163.com>
2025-03-07 21:29:16 +0800

    3-7

--- c++/blackhorse.cpp
+++ c++/blackhorse.cpp
@@ -400,18 +400,20 @@
 //}
 
 
-//2>构造函数和析构函数：
+//2.对象的初始化和清理：
+
+//1>构造函数和析构函数：
 //作用：进行初始化和清理工作
 //系统自动调用，且只调用一次
 //构造函数可以有参数，以此可以重载
 //析构函数不可以有参数
 //如果我们没有自定义构造函数和析构函数，编译器会提供默认函数，且函数为空实现（函数体为空的函数）
 
-//*1.构造函数的分类:
+//2>构造函数的分类:
 //按参数分类：无参构造函数（默认构造函数），有参构造函数
 //按类型分类：普通构造函数，拷贝构造函数
 
-//*2.构造函数的调用：括号法，显示法，隐式转换法
+//3>构造函数的调用：括号法，显示法，隐式转换法
 
 //#include<iostream>
 //using namespace std;
@@ -473,5 +475,175 @@
 //}
 
 
-//*3.拷贝构造函数调用时机：
+//4>拷贝构造函数调用时机：
+//使用一个已经创建好的对象初始化新对象
+//以值传递的形式给函数传参
+//以值方式返回局部对象
 
+
+//5>构造函数调用规则：
+//默认情况下，c++编译器至少给一个类添加3个默认函数：
+//默认构造函数（无参，函数体为空）
+//默认析构函数（无参，函数体为空）
+//默认拷贝构造函数，对属性进行值拷贝
+
+//自定义了有参构造函数，编译器不再提供默认无参构造函数，任提供拷贝构造函数
+//自定义了拷贝构造函数，编译器不再提供其他构造函数
+
+//#include<iostream>
+//using namespace std;
+//class Person
+//{
+//public:
+//	int m_age;
+//	/*Person()
+//	{
+//		cout << "调用默认无参构造函数" << endl;
+//	}*/
+//	/*Person(int age)
+//	{
+//		cout << "调用有参构造函数" << endl;
+//		m_age = age;
+//	}*/
+//	/*Person(const Person& p)
+//	{
+//		cout << "调用拷贝构造函数" << endl;
+//		m_age = p.m_age;
+//	}*/
+//	
+//};
+//
+//int main()
+//{
+//	//Person p1;   //错误：类 "Person" 不存在默认构造函数
+//	//Person p(18);  //错误：没有与参数列表匹配的构造函数 "Person::Person" 实例
+//	return 0;
+//}
+
+
+
+//6>深拷贝和浅拷贝：
+//浅拷贝：简单的赋值拷贝（默认拷贝构造函数是浅拷贝）
+//深拷贝：在堆区重新申请空间，进行拷贝操作
+//***注意：成员属性包括堆区开辟变量时，要自定义拷贝构造函数（深拷贝）
+
+//#include<iostream>
+//using namespace std;
+//class Person
+//{
+//public:
+//	int m_age;
+//	int* m_height;
+//	Person(int age, int height)
+//	{
+//		m_age = age;
+//		m_height = new int(height);
+//	}
+//	Person(const Person& p)
+//	{
+//		//默认拷贝构造函数是浅拷贝（如下代码）
+//		//如果成员属性有在堆区开辟的变量，会直接赋值相同的地址
+//		//析构函数中，同一块空间被释放两次，发生错误
+//		/*m_age = p.m_age;
+//		m_height = p.m_height;*/
+//
+//		//深拷贝：
+//		m_age = p.m_age;
+//		m_height = new int(*p.m_height);
+//	}
+//	~Person()
+//	{
+//		//析构函数的作用：释放在堆区开辟的空间
+//		if (m_height != NULL)
+//		{
+//			delete m_height;
+//			m_height = NULL;
+//		}
+//	}
+//};
+//int main()
+//{
+//	Person p1(19, 158);
+//	cout << "pl: " << p1.m_age << " " << *p1.m_height << endl;
+//	Person p2(p1);
+//	cout << "p2: " << p2.m_age << " " << *p2.m_height << endl;
+//	return 0;
+//}
+
+
+
+//7>初始化成员列表：-->尽量使用初始化成员列表来初始化
+//语法：构造函数（）：属性1（值1），属性2（值2）...
+
+//#include<iostream>
+//using namespace std;
+//class Person
+//{
+//public:
+//	int m_a;
+//	int m_b;
+//	int m_c;
+//	Person(int a, int b, int c) :m_a(a), m_b(b), m_c(c) { ; }
+//
+//};
+//int main()
+//{
+//	Person p(10, 20, 30);
+//	cout << p.m_a << " " << p.m_b << " " << p.m_c << endl;
+//		return 0;
+//}
+
+
+
+//8>类对象作为类成员：
+//当一个A类中包含其他B类对象时：
+//先调用B类的构造函数，再调用A类的构造函数
+//先调用A类的析构函数，再调用B类的析构函数
+
+//类对象作为类成员初始化时：必须使用成员初始化列表
+//类对象成员不能在构造函数体内通过赋值初始化
+//初始化列表会调用成员对象的构造函数，确保它们在构造函数体执行之前已经被正确初始化
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Phone
+//{
+//public:
+//	int m_id;
+//	string m_pname;
+//	Phone(int id, string pname) :m_id(id), m_pname(pname)
+//	{
+//		cout << "Phone的构造函数调用" << endl;
+//	}
+//	~Phone()
+//	{
+//		cout << "Phone的析构函数调用" << endl;
+//	}
+//};
+//class Person
+//{
+//public:
+//	string m_name;
+//	Phone m_phone;   //对象成员
+//
+//	//注意对象成员的初始化  
+//	Person(string name, int id, string pname) :m_name(name), m_phone(id, pname)  
+//	{
+//		cout << "Person的构造函数调用" << endl;
+//	}
+//	~Person()
+//	{
+//		cout << "Person的析构函数调用" << endl;
+//	}
+//
+//};
+//int main()
+//{
+//	Person("lyl", 19, "vivo");
+//	return 0;
+//}
+
+
+
+//9>静态成员：
